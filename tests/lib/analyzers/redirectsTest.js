08badf6481819e6ba27786005f5439ada366e054
Dainis <dainis.tillers@gmail.com>
2016-01-21 18:31:56 +0200

    When checking redirects follow redirect chain from target URI to last redirect and compare only those redirects(#23)

--- tests/lib/analyzers/redirectsTest.js
+++ tests/lib/analyzers/redirectsTest.js
@@ -9,6 +9,9 @@ describe('redirects.js', () => {
   beforeEach(() => {
     redirects = new Redirects();
     campaign = {
+      details : {
+        targetURI : 'http://resource2'
+      },
       _id : 'some id'
     };
   });
@@ -22,20 +25,20 @@ describe('redirects.js', () => {
         }
       }, {
         redirects : {
-          'http://resource1' : 'http://redirect1',
+          'http://resource1' : 'http://redirect4',
           'http://resource2' : 'http://redirect3'
         }
       }], (error, result) => {
         should.not.exist(error);
         result.correct.should.be.equal(false);
         result.diff.should.be.eql({
-          'http://resource2' : ['http://redirect2', 'http://redirect3']
+          'http://resource2' : ['http://redirect2/', 'http://redirect3/']
         });
         done();
       });
     });
 
-    it('should return true if all redirects match', (done) => {
+    it('should return true if page redirect matches even if secondary redirects dont', (done) => {
       redirects.analyse(campaign, [{
         redirects : {
           'http://resource1' : 'http://redirect1',
@@ -45,6 +48,44 @@ describe('redirects.js', () => {
         redirects : {
           'http://resource1' : 'http://redirect1',
           'http://resource2' : 'http://redirect2'
+        }
+      }], (error, result) => {
+        should.not.exist(error);
+        result.correct.should.be.equal(true);
+        result.diff.should.be.eql({});
+        done();
+      });
+    });
+
+    it('should not go in endless loop if there is loop in redirects', (done) => {
+      redirects.analyse(campaign, [{
+        redirects : {
+          'http://resource1' : 'http://resource2',
+          'http://resource2' : 'http://resource1'
+        }
+      }, {
+        redirects : {
+          'http://resource1' : 'http://resource2',
+          'http://resource2' : 'http://resource1'
+        }
+      }], (error, result) => {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should drop query parameters when comparing unique addresses', (done) => {
+      redirects.analyse(campaign, [{
+        redirects : {
+          'http://resource2' : 'http://redirect2?test=1',
+          'http://redirect2?test=1' : 'http://redirect2?test=3',
+          'http://redirect2?test=3' : 'http://resource1/something?test=4'
+        }
+      }, {
+        redirects : {
+          'http://resource2' : 'http://redirect1?test=1',
+          'http://redirect1?test=1' : 'http://redirect2?test=3',
+          'http://redirect2?test=3' : 'http://resource1/something?test=5'
         }
       }], (error, result) => {
         should.not.exist(error);
